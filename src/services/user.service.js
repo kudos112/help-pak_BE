962c14be0dd473b5a9d21469ee4c170e4aa53584
Abdul Quddoos <53150433+abdul-quddous@users.noreply.github.com>
2022-05-06 22:58:09 +0500

    fixed user ngo merge issue

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,6 @@
 const httpStatus = require('http-status');
 const { User, Ngo } = require('../models');
 const ApiError = require('../utils/ApiError');
-const { sendEmail } = require('./email.service');
 
 /**
  * Create a user
@@ -12,7 +11,10 @@ const createUser = async (userBody) => {
   if ((await User.isEmailTaken(userBody.email)) || (await Ngo.isEmailTaken(userBody.email))) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
   }
-  if (userBody.userType === 'NGO') userBody = { ...userBody, role: 'ngo' };
+  if (userBody.userType === 'NGO') {
+    if (userBody.regNo) userBody = { ...userBody, role: 'ngo' };
+    else throw new ApiError(httpStatus.BAD_REQUEST, 'Ngo Registration number is requried to signup');
+  }
   const user = await User.create(userBody);
   return user;
 };
